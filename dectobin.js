53e38cbbcdc721e7d149931fb98c0ab40e7e3855
Angel Vanegas <angelman@gmail.com>
2016-04-28 15:53:36 -0400

    javascript format fix

--- dectobin.js
+++ dectobin.js
@@ -1,15 +1,15 @@
 inlets = 1
 outlets = 16
 
-function msg_int(num){
-  var str = (num).toString(2);
-  while(str.length < 16){
-    str = '0' + str;  
-  }
-  
-  for(var i = 0; i < 16; i++){
-    outlet(i, parseInt(str[i]));
-  }
-  
- 
-} 
+function msg_int(num) {
+    var str = (num).toString(2);
+    while (str.length < 16) {
+        str = '0' + str;
+    }
+
+    for (var i = 0; i < 16; i++) {
+        outlet(i, parseInt(str[i]));
+    }
+
+
+}
